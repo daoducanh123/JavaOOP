f0bc8a48adce78904f6daafdb0e842422151dbf9
Đào Đức Anh <daoducanh1000@gmail.com>
2026-08-28 19:34:20 +0700

    TongPhanSo

--- Codeptit/src/codeptit/J04003_Phanso.java
+++ Codeptit/src/codeptit/J04003_Phanso.java
@@ -11,28 +11,52 @@ import java.util.Scanner;
 
 
 public class J04003_Phanso {
+    
+    public static long gcd (long a, long b){
+        while (b>0){
+            long tmp = a;
+            a = b;
+            b = tmp % b;
+        }
+        return a;
+    }
+
+    
     public static class PhanSo{
-        private int mau,tu;
+        private long mau,tu;
+        public static long tongTu, tongMau;
         
-        public PhanSo(int tu, int mau){
+        public PhanSo(){
+            
+        }
+        
+        public PhanSo(long tu, long mau){
             this.tu = tu;
             this.mau = mau;
         }
         
-        public int calculate(){
+        public static void calculate(PhanSo p, PhanSo q){
+            tongMau = p.mau * q.mau;
+            tongTu = p.tu * q.mau + q.tu * p.mau;
             
+            long GCD = gcd(tongTu,tongMau);
+            tongMau /= GCD;
+            tongTu /= GCD;
+
         }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        int tuP = sc.nextInt();
-        int mauP = sc.nextInt();
+        long tuP = sc.nextLong();
+        long mauP = sc.nextLong();
         
-        int tuQ = sc.nextInt();
-        int mauQ = sc.nextInt();
-                
+        long tuQ = sc.nextLong();
+        long mauQ = sc.nextLong();
+        
         PhanSo p = new PhanSo(tuP, mauP);
         PhanSo q = new PhanSo(tuQ, mauQ);
-
+        
+        PhanSo.calculate(p, q);
+        System.out.println(String.format("%d/%d", PhanSo.tongTu, PhanSo.tongMau));    
     }
 }
