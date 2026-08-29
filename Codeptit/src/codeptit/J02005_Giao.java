b27999678185a9300a2857577aa7fb2721e05365
Đào Đức Anh <daoducanh1000@gmail.com>
2026-08-29 18:01:43 +0700

    Đề bài: J02005 — GIAO CỦA HAI DÃY SỐ

---
+++ Codeptit/src/codeptit/J02005_Giao.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package codeptit;
+import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
+/**
+ *
+ * @author DAGAMING
+ */
+public class J02005_Giao {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[m];
+        
+        boolean[] visitedA = new boolean[105];
+        boolean[] visitedB = new boolean[105];
+        
+        Arrays.fill(visitedA, false);
+        Arrays.fill(visitedB, false);
+
+        for (int i = 0; i < n; ++i){
+            int x = sc.nextInt();
+            if (!visitedA[x]){
+                a[i] = x;
+                visitedA[x] = true;
+            }
+        }
+        
+        for (int i = 0; i < m; ++i){
+            int x = sc.nextInt();
+            if (!visitedB[x]){
+                b[i] = x;
+                visitedB[x] = true;
+            }
+        }
+        
+        for (int i = 0; i < 104 ; ++i){
+            if (visitedA[i] == visitedB[i] && visitedA[i] == true){
+                System.out.print(i + " ");
+            }
+        }
+        
+    }
+}
