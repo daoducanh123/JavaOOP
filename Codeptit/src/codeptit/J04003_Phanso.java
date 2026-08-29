7b8da3c02fc93efb6df5dd436255bf1fe69559c9
Đào Đức Anh <daoducanh1000@gmail.com>
2026-08-29 14:39:14 +0700

    PhanSo

--- Codeptit/src/codeptit/J04003_Phanso.java
+++ Codeptit/src/codeptit/J04003_Phanso.java
@@ -11,5 +11,35 @@ import java.util.Scanner;
 
 
 public class J04003_Phanso {
- 
+    public static int gcd (int a, int b){
+        while (b > 0){
+            int tmp = a;
+            a = b;
+            b = tmp % b;
+        }
+        return a;
+    }
+    
+    public static class PhanSo{
+        private int tu, mau;
+        
+        public PhanSo(int a, int b){
+            this.tu = a;
+            this.mau = b;
+        }   
+        public static void Rutgon(PhanSo p){
+            int GCD = gcd (p.tu, p.mau);
+            p.tu /= GCD;
+            p.mau /= GCD;
+        }
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        PhanSo p = new PhanSo(a,b);
+        PhanSo.Rutgon(p);
+        System.out.println(p.tu + "/" + p.mau);
+    }
 }
